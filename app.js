9f9df2183a99fae28d3f490c3ef4cbd5c62e8361
delfina <delfinapazzavalia@gmail.com>
2023-07-06 19:27:48 -0300

    CRUD

--- app.js
+++ app.js
@@ -1,10 +1,63 @@
-const mongoose = require('mongoose')
+const mongoose = require("mongoose");
 
-const url = 'mongodb://localhost:27017/base_personas' 
+const url = "mongodb://localhost:27017/base_personas ";
 
 mongoose.connect(url, {
-    usenewurlparse: true,
-    useunifiedtopology: true,
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
 })
-.then(db => console.log('db is connected'))
-.cathc(err => console.error(err)) 
+.then((db) => console.log('DB is connected'))
+.catch((err) => console.error(err));
+
+const personasSchema = new mongoose.Schema({
+	nombre: String,
+	edad: Number,
+	profesión: String,
+}, {versionKey: false}); 
+
+const PersonasModel = mongoose.model("personas", personasSchema); 
+
+//CRUD
+
+//READ
+
+// const mostrarDatos = async () => {
+//     const personas = await PersonasModel.find();
+//     console.log(personas);
+// };
+// mostrarDatos(); 
+
+//CREATE
+
+// const createPersona = async () => {
+//     const persona = new PersonasModel({
+//         nombre: "Juan",
+//         edad: 25,
+//         profesión: "scrum master",
+// });
+//     const personaNueva = await persona.save();
+//     console.log(personaNueva);
+// }; 
+// createPersona();
+
+//UPDATE
+
+// const actualizarPersona = async (id) => {
+//     const persona = await PersonasModel.updateOne({_id: id}, {
+//         $set: {
+//             nombre: "Pedro",
+//             edad: 22,
+//             profesión: "ux designer",
+//             }
+//     });
+//         console.log(persona);
+// };
+// actualizarPersona('63644af901663f1b11b72e08'); 
+
+//DELETE
+
+// const eliminarPersona = async (id) => {
+//     const persona = await PersonasModel.deleteOne({_id: id});
+//     console.log(persona);
+// };
+// eliminarPersona('63644af901663f1b11b72e08');
